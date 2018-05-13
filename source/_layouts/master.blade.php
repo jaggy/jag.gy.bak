4d0b9da7b60031e281f3d6ee3c6d89981dae6324
Jaggy Gauran <jaggy@artisan.studio>
2018-05-13 20:11:22 +0800

    Add google analytics.

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -51,6 +51,16 @@
 
         <script src="{{ mix('js/main.js') }}"></script>
 
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-60730521-1"></script>
+        <script>
+         window.dataLayer = window.dataLayer || [];
+         function gtag(){dataLayer.push(arguments);}
+         gtag('js', new Date());
+
+         gtag('config', 'UA-60730521-1');
+        </script>
+
         <script>
          (function(d) {
              var config = {
