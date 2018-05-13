7405464c0e61315da0a06f6eabe520e9ff318cd0
Jaggy Gauran <jaggy@artisan.studio>
2018-05-13 12:59:57 +0800

    Remove the defaults.

--- source/_assets/js/thoughts.js
+++ source/_assets/js/thoughts.js
@@ -2,4 +2,4 @@ import Prism from 'prismjs'
 
 const loadLanguages = require('prismjs/components/index.js');
 
-loadLanguages(['javascript', 'css', 'php', 'markup', 'makefile']);
+loadLanguages(['javascript', 'php', 'makefile']);
