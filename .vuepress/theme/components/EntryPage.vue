596be2d9d01c0f83253f7a2bd744a96d37ba4706
Jaggy Gauran <jaggy@artisan.studio>
2019-11-02 05:29:03 +0800

    Add styles for some rich text goodness.

--- .vuepress/theme/components/EntryPage.vue
+++ .vuepress/theme/components/EntryPage.vue
@@ -1,15 +1,19 @@
 <template>
 <article class="p-entry">
-  <header class="p-entry__header [ py-6 mb-6 ]">
-    <div class="o-content-container o-wrap">
+  <header class="p-entry__header [ py-12 mb-6 ]">
+    <div class="o-content-container o-wrap o-wrap--small">
+      <ul class="p-entry__tags [ -mx-2 mb-6 ]">
+        <li class="[ mx-2 ]" v-for="tag in tags" :key="tag">{{ tag }}</li>
+      </ul>
+
       <h1 class="p-entry__title">{{ $page.title }}</h1>
 
-      Published {{ date.format('MMMM d, YYYY') }}
+      <p class="[ mt-2 ms-sm ]">Published {{ date.format('MMMM d, YYYY') }}</p>
     </div>
   </header>
 
-  <div class="o-content-container o-wrap">
-    <Content  class="p-entry__content o-rich-text" />
+  <div class="o-content-container o-wrap o-wrap--small">
+    <Content class="p-entry__content o-rich-text" ref="content" />
   </div>
 </article>
 </template>
@@ -21,13 +25,37 @@ export default {
   computed: {
     date () {
       return moment(this.$page.frontmatter.date)
+    },
+
+    tags () {
+      const tags = this.$page.frontmatter.tags || []
+
+      if (! Array.isArray(tags)) {
+        return [tags]
+      }
+
+      return tags
+    },
+
+    usesLede () {
+      return this.$page.frontmatter.lede || false
+    }
+  },
+
+  mounted() {
+    if (this.usesLede) {
+      const lede = this.$refs.content.$el.firstChild
+
+      if (lede.tagName === 'P') {
+        lede.classList.add('lede')
+      }
     }
   }
 }
 </script>
 
 <style lang="postcss">
-.p-entry__title {
-  @apply ms-4xl;
+.p-entry__tags {
+  @apply uppercase ms-sm flex opacity-75;
 }
 </style>
--- .vuepress/theme/tailwind.config.js
+++ .vuepress/theme/tailwind.config.js
@@ -1,8 +1,17 @@
 module.exports = {
   theme: {
+    colors: {
+      black: '#4b4f56',
+    },
+
     extend: {
+      letterSpacing: {
+        normal: '-0.02em'
+      },
+
       fontFamily: {
         sans: [
+          // 'neue-haas-unica',
           '-apple-system',
           'BlinkMacSystemFont',
           '"Segoe UI"',
@@ -23,16 +32,20 @@ module.exports = {
   plugins: [
     require('tailwindcss-modularscale')({
       sizes: [
+        { size: 'xs', value: -2 },
         { size: 'sm', value: -1 },
         { size: 'base', value: 0 },
         { size: 'lg', value: 1 },
         { size: 'xl', value: 2 },
         { size: '2xl', value: 3 },
         { size: '3xl', value: 4 },
-        { size: '4xl', value: 5 }
+        { size: '4xl', value: 5 },
+        { size: '5xl', value: 6 },
+        { size: '6xl', value: 7 },
+        { size: '7xl', value: 8 },
       ],
       base:  18,
-      ratio: 1.212, // Perfect Fourth
+      ratio: 1.2,
       unit: 'px',
     })
   ]
